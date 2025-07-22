cfd82baec95ef391b18e5e97d95d123c7d987a5a
GeorgeTYQ <157372345+GeorgTYQ@users.noreply.github.com>
2025-07-22 20:01:38 +1000

    feat:footer

---
+++ src/app/(landing-page)/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <footer className='w-full border-t border-gray-800 bg-gray-900 text-white'>
+      <div className="container">
+        <div className="flex justify-center items-center py-8">
+          <div className="text-gray-500 text-center">
+            <p>&copy; 2025 George Porfolio. All rights reserved</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
--- src/app/(landing-page)/components/Header.jsx
+++ src/app/(landing-page)/components/Header.jsx
@@ -4,14 +4,14 @@ import React from 'react'
 
 export const Navbar = () => {
   return (
-    <div className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212]/100">
+    <div className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-black/90">
       <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
-        <Link href="/" className="text-2xl md:text-3xl text-white font-normal">
-        WEB
+        <Link href="/" className="text-xl md:text-2xl text-white font-normal">
+        GEORGE WEBSITE
         </Link>
         <div className="menu hidden md:block md:w-auto" id="navbar">
           <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
-            <li key="about" className='hover:text-slate-200'><Link href="/about">About</Link></li>
+            {/* <li key="about" className='hover:text-slate-200'><Link href="/about">About</Link></li> */}
             <li key="projects" className='hover:text-slate-200'><Link href="#projects">Projects</Link></li>
             <li key="blog" className='hover:text-slate-200'><Link href="#blog">Blog</Link></li>
           </ul>
