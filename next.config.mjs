20c0d56d02db037c7539c775e422816069130a8a
GeorgeTYQ <157372345+GeorgTYQ@users.noreply.github.com>
2025-07-27 22:48:28 +1000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,15 +8,13 @@ const nextConfig = {
   webpackDevMiddleware: (config) => {
     config.watchOptions = {
       ignored: [
-        /node_modules([\\/])/, // ignore node_modules only
-        /\.next([\\/])/, // ignore .next build folder
-        /public([\\/])/, // okay to ignore static assets
+        '**/node_modules/**',
+        '**/.next/**',
+        '**/public/**',
       ],
     };
     return config;
   },
 };
 
-// Make sure you wrap your `nextConfig`
-// with the `withPayload` plugin
 export default withPayload(nextConfig);
